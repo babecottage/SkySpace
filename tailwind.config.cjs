1d32873897d4ae939b37cd24b39160ba99ad8a79
Jem Gold <591643+jemgold@users.noreply.github.com>
2023-04-26 23:24:22 -0700

    stylin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
+      pageBackground: "var(--pageBackground)",
       background: "var(--background)",
       text: "var(--text)",
       border: "var(--border)",
